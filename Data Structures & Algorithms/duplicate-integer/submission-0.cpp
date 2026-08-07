5c63b88833a3b5ee4c3fdc67b5bea49b2f34e046
Sahil Kumar <sahil.kumar.aiml27@heritageit.edu.in>
2026-08-08 01:59:28 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        for(int x : nums) {
+            if(mp.find(x) != mp.end()) return true;
+            mp[x]++;
+        }
+        return false;
+    }
+};
